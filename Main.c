b97ccb1fc9f07f35b2d5adffdbb32f467293035b
Yihong Liu <yihongliu34@outlook.com>
2019-01-16 17:01:12 -0500

    started making snake game

--- Main.c
+++ Main.c
@@ -2,52 +2,26 @@
 #include<stdlib.h>
 #include "log.h"
 
-struct Node
-{
-	struct Node *next;
-	int data;
-};
-
-struct Node* new_node(int data)
-{
-	struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-	node->data = data;
-	node->next = NULL;
-	return node;
-}
-int is_empty(struct Node* root)
-{
-	return !root;	
-}
-
-int push(struct Node** root, int data)
-{
-	struct Node* node = new_node(data);
-	node->next = *root;
-	*root = node;
-	printf("%d pushed to stack", data);	
-}
-int pop(struct Node** root)
-{
-	struct Node* temp = *root;
-	*root = (*root)->next;
-	int pop = temp->data;
-	free(temp);
-
-	return pop;
-}
-int peek(struct Node* root)
-{
-	return root->data;
-}
-
 int main();
+
+void draw(int x, int y, char px);
 
 int main()
 {
-	struct Node* head = (struct Node*)malloc(sizeof(struct Node));
-	head->data = 10;
-	push(&head, 20);
-	printf("%d", peek(head));
+	draw(3, 3, '#');
 	return 0;
+}
+
+void draw(int x, int y, char px)
+{
+	system("clear");
+	for(int i = 0; i<x; i++)
+	{
+		for(int j = 0; j<x; j++)
+		{
+			printf("#");
+		}
+		printf("\r\n");
+	}
+	//printf("%c", px);
 }
