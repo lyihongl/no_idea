37390436c83eaa7f87cc7b8e6f2941c2b62571c3
Yihong Liu <yihongliu00@gmail.com>
2019-01-16 22:56:17 -0500

    figured out how to do io in c, added a bunch of drawing functions

--- Main.c
+++ Main.c
@@ -1,27 +1,69 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<windows.h>
 #include "log.h"
+
+#define X_SIZE 50
+#define Y_SIZE 30
+
+#define KEYPRESSED -32768
+#define LIMITSCANCODE 255
 
 int main();
 
-void draw(int x, int y, char px);
+struct data_grid
+{
+	int x[X_SIZE];
+	int y[Y_SIZE];
+};
+
+void init_grid(struct data_grid *grid);
+void draw_grid(struct data_grid *grid);
+void edit_grid(struct data_grid *grid, int x, int y, int val);
 
 int main()
 {
-	draw(3, 3, '#');
+	do
+	{
+		if(GetAsyncKeyState(0x57))
+		{
+			printf("yeet");
+		}
+	}while(1);
 	return 0;
 }
+void edit_grid(struct data_grid *grid, int x, int y, int val)
+{
+	if(val != 1 && val != 0)
+	{
+		return;
+	}
+	grid->x[x] = val;
+	grid->y[y] = val;
+}
+void init_grid(struct data_grid *grid)
+{
+	for(int i = 0; i<Y_SIZE; grid->y[i] = 0, ++i);
+	for(int i = 0; i<X_SIZE; grid->x[i] = 0, ++i);
+}
 
-void draw(int x, int y, char px)
+void draw_grid(struct data_grid *grid)
 {
 	system("clear");
-	for(int i = 0; i<x; i++)
+	for(int i = 0; i<Y_SIZE; i++)
 	{
-		for(int j = 0; j<x; j++)
+		for(int j = 0; j<X_SIZE; j++)
 		{
-			printf("#");
+			if(grid->x[j] && grid->y[i])
+			{
+				//printf("%d %d", i, j);
+				printf("%c", '#');
+			}
+			else
+			{
+				printf("  ");
+			}
 		}
 		printf("\r\n");
 	}
-	//printf("%c", px);
 }
